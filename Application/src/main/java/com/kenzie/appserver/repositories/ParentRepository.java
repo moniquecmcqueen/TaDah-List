98b5232a9c69350ae404a717beaec3cf1557abb7
rebeccamharris <rebecca.harris@mykenzie.snhu.edu>
2023-06-21 19:43:50 -0400

    parent,child and task repositories created(not completed)

--- Application/src/main/java/com/kenzie/appserver/repositories/ParentRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/ParentRepository.java
@@ -2,9 +2,10 @@ package com.kenzie.appserver.repositories;
 
 import com.kenzie.appserver.repositories.model.ExampleRecord;
 
+import com.kenzie.appserver.service.model.Parent;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 @EnableScan
-public interface ExampleRepository extends CrudRepository<ExampleRecord, String> {
+public interface ParentRepository extends CrudRepository<Parent, String> {
 }
