013d59d55dac226b50b7eee20ee0250c69fd914f
rebeccamharris <rebecca.harris@mykenzie.snhu.edu>
2023-06-21 19:34:18 -0400

    removed methods from tadahtasklist and added them to task controller per convo with monique

--- Application/src/main/java/com/kenzie/appserver/controller/TaskController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/TaskController.java
@@ -1,4 +1,47 @@
 package com.kenzie.appserver.controller;
 
+import com.kenzie.appserver.service.model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskController {
+
+    private List<Task> tasks;
+
+    public void addTask(Task task){
+        tasks.add(task);
+    }
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+    public List<Task> getAllTasks(){
+        return tasks;
+    }
+    public List<Task> getAllCompletedTasks() {
+        List<Task> theCompletedTasks = new ArrayList<>();
+        for(Task task : tasks) {
+            if(task.isCompleted()) {
+                theCompletedTasks.add(task);
+            }
+        }
+        return theCompletedTasks;
+    }
+    public List<Task> getAllIncompletedTasks() {
+        List<Task> theIncompletedTasks = new ArrayList<>();
+        for(Task task : tasks) {
+            if(!task.isCompleted()) {
+                theIncompletedTasks.add(task);
+            }
+        }
+        return theIncompletedTasks;
+    }
+    public void markTaskAsCompleted(Task task){
+        task.setCompleted(true);
+    }
+    public void markTaskAsIncompleted(Task task) {
+        task.setCompleted(false);
+    }
+}
+
 }
--- Application/src/main/java/com/kenzie/appserver/service/model/TaDahTaskList.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/TaDahTaskList.java
@@ -6,42 +6,11 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class TaDahTaskList {
-private List<Task> tasks;
+    private List<Task> tasks;
 
-public TaDahTaskList() {
-    tasks = new ArrayList<>();
-}
-public void addTask(Task task){
-    tasks.add(task);
-}
-public void removeTask(Task task) {
-    tasks.remove(task);
-}
-public List<Task> getAllTasks(){
-    return tasks;
-}
-public List<Task> getAllCompletedTasks() {
-    List<Task> theCompletedTasks = new ArrayList<>();
-    for(Task task : tasks) {
-        if(task.isCompleted()) {
-            theCompletedTasks.add(task);
-        }
+    public TaDahTaskList() {
+        tasks = new ArrayList<>();
     }
-    return theCompletedTasks;
-}
-public List<Task> getAllIncompletedTasks() {
-    List<Task> theIncompletedTasks = new ArrayList<>();
-    for(Task task : tasks) {
-        if(!task.isCompleted()) {
-            theIncompletedTasks.add(task);
-        }
-    }
-    return theIncompletedTasks;
-}
-public void markTaskAsCompleted(Task task){
-    task.setCompleted(true);
-}
-public void markTaskAsIncompleted(Task task) {
-    task.setCompleted(false);
-}
+
+    //removed methods and added them to controller class-rebecca
 }
