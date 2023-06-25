c515a1e2a657f9d7f0ff8e71b75c28d1cce543ab
MoniqueCMcQueen <monique.mcqueen@mykenzie.snhu.edu>
2023-06-25 15:46:21 -0400

    resolved issues

--- Application/src/main/java/com/kenzie/appserver/repositories/ChildRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/ChildRepository.java
@@ -2,7 +2,6 @@ package com.kenzie.appserver.repositories;
 
 import com.kenzie.appserver.repositories.model.TaskRecord;
 import com.kenzie.appserver.service.model.Child;
-import com.kenzie.appserver.service.model.Parent;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ChildRepository extends CrudRepository<TaskRecord, String> {
