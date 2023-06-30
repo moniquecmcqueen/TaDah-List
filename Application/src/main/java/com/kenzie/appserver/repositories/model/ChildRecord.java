40e187adddd98d3077c4483560816b67994864ec
mchone1996 <113073952+mchone1996@users.noreply.github.com>
2023-06-30 03:09:01 -0700

    added fetch for the tasklist.html, deleted dupliacte child record??

--- Application/src/main/java/com/kenzie/appserver/repositories/model/ChildRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/ChildRecord.java
@@ -5,57 +5,6 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 import java.util.Objects;
-
- brandon2
-@DynamoDBTable(tableName = "TaDahChild")
-public class ChildRecord {
-
-    private String childId;
-
-    @DynamoDBAttribute(attributeName = "childUsername")
-    private String childUsername;
-
-    @DynamoDBAttribute(attributeName = "taskId")
-    private String taskId;
-  
- @DynamoDBHashKey(attributeName = "childId")
-    public String getChildId() {
-        return childId;
-    }
-
-    public void setChildId(String childId) {
-        this.childId = childId;
-    }
-
-    public String getChildUsername() {
-        return childUsername;
-    }
-
-    public void setChildUsername(String childUsername) {
-        this.childUsername = childUsername;
-    }
-
-    public String getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(String taskId) {
-        this.taskId = taskId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ChildRecord)) return false;
-        ChildRecord that = (ChildRecord) o;
-        return Objects.equals(childId, that.childId) && Objects.equals(childUsername, that.childUsername);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(childId, childUsername);
-    }
-}
     @DynamoDBTable(tableName = "TaDahChild")
 
     public class ChildRecord {
@@ -82,10 +31,13 @@ public class ChildRecord {
         public void setChildUsername(String childUsername) {
             this.childUsername = childUsername;
         }
-        @DynamoDBAttribute(attributeName = "taskId")
-        public String getTaskId() {return taskId; }
 
-        public void setTaskId (String taskId){
+        @DynamoDBAttribute(attributeName = "taskId")
+        public String getTaskId() {
+            return taskId;
+        }
+
+        public void setTaskId(String taskId) {
             this.taskId = taskId;
         }
 
@@ -101,3 +53,4 @@ public class ChildRecord {
         public int hashCode() {
             return Objects.hash(childId, childUsername);
         }
+    }
