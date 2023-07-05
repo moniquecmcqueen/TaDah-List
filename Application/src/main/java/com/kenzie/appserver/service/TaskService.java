481e83e51c41a82d373ad7c9acf2635751fee021
rebeccamharris <rebecca.harris@mykenzie.snhu.edu>
2023-07-05 17:28:54 -0400

    clean up

--- Application/src/main/java/com/kenzie/appserver/service/TaskService.java
+++ Application/src/main/java/com/kenzie/appserver/service/TaskService.java
@@ -33,14 +33,13 @@ public class TaskService {
     }
 
 
-
     public List<Task> getAllTasks() {
         List<Task> tasks = new ArrayList<>();
 
         Iterable<TaskRecord> taskIterator = taskRepository.findAll();
 
-        for(TaskRecord record : taskIterator) {
-            tasks.add(new Task(record.getParentUsername(),record.getChildUsername(), record.getTaskId(),
+        for (TaskRecord record : taskIterator) {
+            tasks.add(new Task(record.getParentUsername(), record.getChildUsername(), record.getTaskId(),
                     record.getTaskTitle(),
                     record.getIsCompleted()));
 
@@ -48,9 +47,10 @@ public class TaskService {
 
         return tasks;
     }
+
     public Task addNewTask(Task task) {
         //do i need to retrieve a parent to add a task ? do i need to retrieve a child to add a task
-       // Child child = new Child();
+        // Child child = new Child();
         Parent parent = parentService.findByParentUsername(task.getParentUsername()); //why did i have to add task to get my parentId in there
         TaskRecord taskRecord = new TaskRecord();
         taskRecord.setParentUsername(parent.getParentUsername());
@@ -82,7 +82,7 @@ public class TaskService {
         taskRecord.setIsCompleted(task.getIsCompleted());
         taskRepository.save(taskRecord);
     }
-
+}
 //    public Task markTaskCompleted(String childId) {
 //        //Retrieve the child with the given id from the childservice
 //        //or parent idk
@@ -103,53 +103,3 @@ public class TaskService {
 //        Task = new Parent(parent.getParentId(),parent.,parent.getChildId()
 //                ,true,parent.g(),true);
 //
-//        reservedTicketService.updateReserveTicket(reservedTicket);
-//
-//        //Return a new PurchasedTicket object.
-//        return new  PurchasedTicket(reservedTicket.getConcertId(),reservedTicket.getTicketId(),
-//                reservedTicket.getDateOfReservation(),pricePaid);
-//
-//    }
-//    public PurchasedTicket purchaseTicket(String reservedTicketId, Double pricePaid) {
-//        // Your code here
-//
-//
-//
-//        //If the ticket reservation is null or if the reservation has been closed,
-//        // then throw a ResponseStatusException with the HttpStatus.BAD_REQUEST status and a message
-//        if(reservedTicket == null) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-//        }
-//
-//        if (reservedTicket.getReservationClosed() != null && reservedTicket.getReservationClosed()) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Reservation is Closed");
-//        }
-//        //Create a PurchasedTicketRecord, set the fields,
-//        PurchasedTicketRecord purchasedTicketRecord = new PurchasedTicketRecord();
-//
-//        purchasedTicketRecord.setTicketId(reservedTicketId);
-//        purchasedTicketRecord.setDateOfPurchase(reservedTicket.getDateOfReservation());
-//        purchasedTicketRecord.setConcertId(reservedTicket.getConcertId());
-//        purchasedTicketRecord.setPricePaid(pricePaid);
-//
-//
-//// and store it in the purchaseTicketRepository.
-//        purchaseTicketRepository.save(purchasedTicketRecord);
-////Update the set the reservationClosed and purchasedTicket flag on the reservedTicket to true
-//
-//        reservedTicket = new ReservedTicket(reservedTicket.getConcertId(),reservedTicket.getTicketId(),reservedTicket.getDateOfReservation()
-//                ,true,reservedTicket.getDateReservationClosed(),true);
-//
-//        reservedTicketService.updateReserveTicket(reservedTicket);
-//
-//        //Return a new PurchasedTicket object.
-//        return new  PurchasedTicket(reservedTicket.getConcertId(),reservedTicket.getTicketId(),
-//                reservedTicket.getDateOfReservation(),pricePaid);
-//
-//    }
-}
-
-
-
-
-
