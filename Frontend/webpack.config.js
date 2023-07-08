07ef3f096a9e4430bbe4e9c92f812c6c44bc216d
Brandon McHone <113073952+brandonmchone23@users.noreply.github.com>
2023-07-07 22:07:12 -0700

    Update webpack.config.js
    revert to base

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -1,67 +1,53 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   optimization: {
-    usedExports: true,
+    usedExports: true
   },
   entry: {
-    main: './src/main.js',
-    usernamecheck: './src/UsernameCheck.js',
-    tasklist: './src/tasklist.js',
-    createaccount: './src/CreateAccount.js',
-    // Add more entry points for other JavaScript files
+    examplePage: path.resolve(__dirname, 'src', 'pages', 'examplePage.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
   },
   devServer: {
     https: false,
     port: 8080,
     open: true,
+    //proxy is what tells your frontend where to find the backend and what requests to send there
+    //if you  notice in the example we are sending all requests that start with /example to
+    //http://localhost:5001 which is where the backend is, when sent to the backend it will become
+    //http://localhost:5001/exemple/...
+    //for example if you sent the request /example/bob to the backend, it will be converted into
+    //http://localhost:5001/example/bob and sent to the backend that way.
+    //uncomment the following proxy section to make the example work
+//    proxy: [
+//          {
+//            context: [
+//              '/example',
+//            ],
+//            target: 'http://localhost:5001'
+//          }
+//        ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/tasklist.html',
-      filename: 'tasklist.html',
+      filename: 'index.html',
+      inject: false
     }),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/css/style.css'),
-          to: 'css',
-        },
-        {
-          from: path.resolve(__dirname, 'src/css/tasklist.css'),
-          to: 'css',
-        },
-        {
-          from: path.resolve(__dirname, 'src/sticker.png'),
-          to: '',
-        },
-
-        {
-          from: path.resolve(__dirname, 'src/Tada-sound.mp3'),
-          to: 'audio',
-        },
-        {
-          from: path.resolve(__dirname, 'src/TaDahLogo.png'),
-          to: '',
-        },
-        {
-          from: path.resolve(__dirname, 'src/throwawaytask.mp3'),
-          to: 'audio',
-        },
-        {
-          from: path.resolve(__dirname, 'src/taskadded.mp3'),
-          to: 'audio',
-        },
-
-
-      ],
+          from: path.resolve('src/css'),
+          to: path.resolve("dist/css")
+        }
+      ]
     }),
-    new CleanWebpackPlugin(),
-  ],
-};
+    new CleanWebpackPlugin()
+  ]
+}
